2934498eb9d1c4a5cf0595d56822a1d97eff7823
timothy1205 <timothy120500@gmail.com>
2023-05-30 19:03:24 -0600

    feat: Automatically import blueprints

--- backend/blueprints/api.py
+++ backend/blueprints/api.py
@@ -1,13 +1,23 @@
 from flask import Blueprint
-from backend.blueprints import user
+from glob import glob
+import os.path
+from importlib import import_module
 
 API_NAME = "api"
 API_PATH = f"/{API_NAME}"
+FILE_BLACKLIST = set(["__init__", "api"])
 
 blueprint = Blueprint(API_NAME, __name__, url_prefix=API_PATH)
 
+# Derive blueprints from python files
+python_files = map(lambda path: os.path.basename(path).rstrip(".py"),
+                   glob(os.path.join(os.path.dirname(__file__), "*.py")))
+blueprint_files = filter(lambda file: file not in FILE_BLACKLIST, python_files)
+
 # Register sub blueprints
-blueprint.register_blueprint(user.blueprint)
+for file in blueprint_files:
+    module = import_module("backend.blueprints." + file)
+    blueprint.register_blueprint(module.blueprint)
 
 
 @blueprint.route("/")
