8d4dbd2324edc7aab82187df9b97c110169da88c
timothy1205 <timothy120500@gmail.com>
2023-07-18 16:35:18 -0600

    feat: Better error types

--- backend/blueprints/user.py
+++ backend/blueprints/user.py
@@ -8,7 +8,16 @@ from backend.data.managers.UserManager import (
     UserManager,
     UsernameExistsError,
 )
-from backend.utils import CODE_BAD_REQUEST, make_error, make_success
+from backend.utils import CODE_BAD_REQUEST, make_error, make_success, require_form_keys
+
+
+class NotLoggedIn(Exception):
+    pass
+
+
+class AlreadyLoggedIn(Exception):
+    pass
+
 
 USER_NAME = "user"
 USER_PATH = f"/{USER_NAME}"
@@ -18,22 +27,11 @@ CODE_CONFLICT = 409
 blueprint = Blueprint(USER_NAME, __name__, url_prefix=USER_PATH)
 
 
-def require_credentials(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        if "username" not in request.form or "password" not in request.form:
-            return make_error("Missing username and/or password", CODE_BAD_REQUEST)
-
-        return f(*args, **kwargs)
-
-    return wrapper
-
-
 def require_logged_in(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
         if USER_NAME not in session:
-            return make_error("You must be logged in", CODE_UNAUTHORIZED)
+            return make_error("You must be logged in", CODE_UNAUTHORIZED, NotLoggedIn())
 
         return f(*args, **kwargs)
 
@@ -41,10 +39,13 @@ def require_logged_in(f):
 
 
 @blueprint.route("/login", methods=["POST"])
-@require_credentials
+@require_form_keys(["username", "password"])
 def login():
     if USER_NAME in session:
-        return make_error("Already logged in", CODE_CONFLICT)
+        return make_error(
+            "Already logged in",
+            CODE_CONFLICT,
+        )
 
     username = request.form.get("username")
     password = request.form.get("password")
@@ -53,14 +54,14 @@ def login():
     valid_password, user = user_manager.check_password(username, password)
 
     if not valid_password:
-        return make_error("Invalid credentials", CODE_UNAUTHORIZED)
+        return make_error("Invalid credentials", CODE_UNAUTHORIZED, AlreadyLoggedIn())
 
     session[USER_NAME] = user
     return make_success("Logged in")
 
 
 @blueprint.route("/register", methods=["POST"])
-@require_credentials
+@require_form_keys(["username", "password"])
 def register():
     username = request.form.get("username")
     password = request.form.get("password")
@@ -70,7 +71,7 @@ def register():
     try:
         created = user_manager.create_user(username, password)
     except (UsernameExistsError, InvalidUsernameError, InvalidPasswordError) as e:
-        return make_error(str(e), CODE_UNAUTHORIZED)
+        return make_error(str(e), CODE_UNAUTHORIZED, e)
     if not created:
         return make_error("Could not create!", CODE_UNAUTHORIZED)
 
