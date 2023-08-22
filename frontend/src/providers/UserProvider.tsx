dd74ffd5035a1de988f2445e90bd1def0b6d4572
timothy1205 <timothy120500@gmail.com>
2023-08-22 00:11:06 -0600

    feat: Added UserProvider for shared user data

---
+++ frontend/src/providers/UserProvider.tsx
@@ -0,0 +1,30 @@
+import { PropsWithChildren, createContext, useMemo, useState } from "react";
+
+interface User {
+  username: string;
+  registration_date: string;
+}
+
+type UserData = User | null;
+
+interface UserContextData {
+  user: UserData;
+  setUser: React.Dispatch<React.SetStateAction<UserData>>;
+}
+
+// Should never be null when used, force TS to ignore it
+export const UserContext = createContext<UserContextData>(null!);
+
+export default function UserProvider({ children }: PropsWithChildren) {
+  const [user, setUser] = useState<UserData>(null);
+
+  const cachedContextData = useMemo<UserContextData>(
+    () => ({ user, setUser }),
+    [user, setUser]
+  );
+  return (
+    <UserContext.Provider value={cachedContextData}>
+      {children}
+    </UserContext.Provider>
+  );
+}
