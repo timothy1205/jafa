6313b1fbd6500b7777e8058f78bdd4ab64cb743c
timothy1205 <timothy120500@gmail.com>
2023-08-21 22:26:56 -0600

    feat: Added api handleError function

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,11 @@
 import axios from "axios";
 import config from "./config";
+import { generateToast } from "./utils";
+
+interface APIError {
+  type: string;
+  error: string;
+}
 
 const apiUrl = `${config.backendUrl}/api`;
 
@@ -11,7 +17,22 @@ const api = {
     formData.append("post_id", post_id);
     formData.append("is_like", is_like.toString());
     return axios.post(`${apiUrl}/post/vote`, formData);
-  }
+  },
+
+  handleError: (err: unknown) => {
+    if (!axios.isAxiosError(err)) {
+      console.error(err);
+      return;
+    }
+
+    const responseData = err.response?.data;
+    if (responseData) {
+      const apiError = responseData as APIError;
+      const errMessage = `[${apiError.type}]: ${apiError.error}`;
+      console.error(errMessage);
+      generateToast(errMessage, "error");
+    }
+  },
 };
 
 export default api;
