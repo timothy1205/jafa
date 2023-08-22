844ddba7345288ad750f2381f483ef56c093ad21
timothy1205 <timothy120500@gmail.com>
2023-08-21 22:23:30 -0600

    feat: Added vote functionality

--- frontend/src/components/posts/PostVote/index.tsx
+++ frontend/src/components/posts/PostVote/index.tsx
@@ -3,14 +3,27 @@ import Button from "@mui/material/Button";
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 import "./index.css";
+import api from "../../../services/api";
 
-export default function PostVote() {
+interface PostVoteProps {
+  post_id: string;
+}
+
+export default function PostVote({ post_id }: PostVoteProps) {
+  async function voteAction(is_like: boolean) {
+    try {
+      await api.vote(post_id, is_like);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
     <div className="postvote">
-      <Button>
+      <Button onClick={() => voteAction(true)}>
         <ArrowUpwardIcon className="postvote-upvote"></ArrowUpwardIcon>
       </Button>
-      <Button>
+      <Button onClick={() => voteAction(false)}>
         <ArrowDownwardIcon className="postvote-downvote"></ArrowDownwardIcon>
       </Button>
     </div>
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -6,6 +6,12 @@ const apiUrl = `${config.backendUrl}/api`;
 const api = {
   postList: (subforum?: string) =>
     axios.get(`${apiUrl}/post/list`, { params: { subforum: subforum } }),
+  vote: (post_id: string, is_like: boolean) => {
+    const formData = new FormData();
+    formData.append("post_id", post_id);
+    formData.append("is_like", is_like.toString());
+    return axios.post(`${apiUrl}/post/vote`, formData);
+  }
 };
 
 export default api;
