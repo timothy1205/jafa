84c0160fbde109aa9169512cfe324f8374b34f4e
timothy1205 <timothy120500@gmail.com>
2023-08-21 18:14:44 -0600

    feat: Added Header component

---
+++ frontend/src/components/general/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+export default function Header() {
+  return (
+    <div id="header">
+      <a href="/" id="header-title">
+        Jafa
+      </a>
+    </div>
+  );
+}
