46af9aa5a582a3c948d46b45a8eac2c624562659
timothy1205 <timothy120500@gmail.com>
2023-08-22 00:11:40 -0600

    feat: Render all providers under Providers in Base

--- frontend/src/routes/Base.tsx
+++ frontend/src/routes/Base.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/general/Header";
+import Providers from "../providers/Providers";
 
 function Base() {
   return (
-    <>
+    <Providers>
       <Header />
       <Outlet />
-    </>
+    </Providers>
   );
 }
 
