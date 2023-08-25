d25f19d8a4de57c97f50de8dc564bd84823d66a6
timothy1205 <timothy120500@gmail.com>
2023-08-24 18:56:03 -0600

    feat: Added logging out

--- frontend/src/components/general/UsernameLogin/index.tsx
+++ frontend/src/components/general/UsernameLogin/index.tsx
@@ -1,10 +1,26 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
-import "./index.css";
+import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "../../../providers/UserProvider";
+import { Button } from "@mui/material";
+import { generateToast } from "../../../services/utils";
+import { handleError, logout } from "../../../services/api";
+import "./index.css";
 
 export default function UsernameLogin() {
-  const { user } = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+    } catch (e) {
+      handleError(e);
+    }
+
+    setUser(null);
+    navigate("/");
+    generateToast("Logged out", "success");
+  };
 
   let toRender;
   if (user === null) {
@@ -14,7 +30,12 @@ export default function UsernameLogin() {
       </Link>
     );
   } else {
-    toRender = <p className="usernamelogin-username">{user.username}</p>;
+    toRender = (
+      <>
+        <p className="usernamelogin-username">{user.username}</p>
+        <Button onClick={handleLogout}>Logout</Button>
+      </>
+    );
   }
 
   return <div className="usernamelogin">{toRender}</div>;
