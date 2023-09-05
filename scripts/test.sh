b496ab65351c9a8065b6a31e151931a5a70a9132
timothy1205 <timothy120500@gmail.com>
2023-09-05 15:00:44 -0600

    test: Update test.sh
    Use docker-compose
    Added frontend test command

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 
-IMAGE=$(docker buildx build -q .)
-docker run --rm $IMAGE python -m unittest discover -s backend/tests/  -p "*_test.py"
-docker image rm $IMAGE > /dev/null 2>&1
+DCO="docker compose -f docker-compose.yml -f docker-compose.dev.yml"
+$DCO build
+
+# Backend
+$DCO run --rm jafa-backend python -m unittest discover -s backend/tests/  -p "*_test.py"
+
+# Frontend
+$DCO run --env CI=true --rm --no-deps jafa-frontend yarn --cwd frontend test --passWithNoTests
