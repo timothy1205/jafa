c4ccc31556306414284598ca944bb5d8c0d782ca
timothy1205 <timothy120500@gmail.com>
2023-09-11 16:36:03 -0600

    feat: Added useExposedUserUpdater and useUserUpdater

--- frontend/src/services/utils.ts
+++ frontend/src/services/utils.ts
@@ -1,8 +1,36 @@
 import { toast, TypeOptions } from "react-toastify";
+import { UserContext, UserData } from "../providers/UserProvider";
+import { useContext, useEffect } from "react";
+import { getCurrentUser } from "./api";
 
 export function generateToast(msg: string, type: TypeOptions) {
   toast(msg, {
     position: toast.POSITION.BOTTOM_CENTER,
     type: type,
   });
+}
+
+export function useExposedUserUpdater() {
+  const { setUser, user } = useContext(UserContext);
+
+  const updateUser = async () => {
+    // Skip if we're already logged in
+    if (user !== null) return;
+
+    const userRes = await getCurrentUser();
+    const userData = userRes.data as UserData;
+    setUser(userData);
+  };
+
+  return updateUser;
+}
+
+export function useUserUpdater() {
+  const updateUser = useExposedUserUpdater();
+
+  useEffect(() => {
+    updateUser();
+    // Check user once on startup
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 }
