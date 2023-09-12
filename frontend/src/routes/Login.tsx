53a665a245d6b058175feaaf64c0c2dda2a9fbb0
timothy1205 <timothy120500@gmail.com>
2023-09-11 23:09:25 -0600

    fix: Redirect to root if logged

--- frontend/src/routes/Login.tsx
+++ frontend/src/routes/Login.tsx
@@ -1,6 +1,13 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
 import LoginRegister from "../components/login/LoginRegister";
+import { UserContext } from "../providers/UserProvider";
 
 function Login() {
+  const { user } = useContext(UserContext);
+
+  if (user !== null) return <Navigate to="/" replace={true} />;
+
   return <LoginRegister />;
 }
 
